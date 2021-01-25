116429f9c6ebfefdff0f74ac014012cc78bb19c7
Benjamin Park <parkbenjamin2@gmail.com>
2021-01-24 23:02:43 -0500

    ncurses

--- src/ext/libncurses.cpp
+++ src/ext/libncurses.cpp
@@ -60,29 +60,10 @@ ROSSA_EXT_SIG(_delwin, args, token, hash, stack_trace)
 	return sym_t::Number(number_t::Long(delwin(win)));
 }
 
-ROSSA_EXT_SIG(_refresh, args, token, hash, stack_trace)
-{
-	return sym_t::Number(number_t::Long(refresh()));
-}
-
 ROSSA_EXT_SIG(_wrefresh, args, token, hash, stack_trace)
 {
 	auto win = COERCE_PTR(args[0].getPointer(token, stack_trace), WINDOW);
 	return sym_t::Number(number_t::Long(wrefresh(win)));
-}
-
-ROSSA_EXT_SIG(_printw, args, token, hash, stack_trace)
-{
-	const std::string s = args[0].getString(token, stack_trace);
-	return sym_t::Number(number_t::Long(printw(s.c_str())));
-}
-
-ROSSA_EXT_SIG(_mvprintw, args, token, hash, stack_trace)
-{
-	const int y = args[0].getNumber(token, stack_trace).getLong();
-	const int x = args[1].getNumber(token, stack_trace).getLong();
-	const std::string s = args[2].getString(token, stack_trace);
-	return sym_t::Number(number_t::Long(mvprintw(y, x, s.c_str())));
 }
 
 ROSSA_EXT_SIG(_wprintw, args, token, hash, stack_trace)
@@ -92,48 +73,11 @@ ROSSA_EXT_SIG(_wprintw, args, token, hash, stack_trace)
 	return sym_t::Number(number_t::Long(wprintw(win, s.c_str())));
 }
 
-ROSSA_EXT_SIG(_mvwprintw, args, token, hash, stack_trace)
-{
-	auto win = COERCE_PTR(args[0].getPointer(token, stack_trace), WINDOW);
-	const int y = args[1].getNumber(token, stack_trace).getLong();
-	const int x = args[2].getNumber(token, stack_trace).getLong();
-	const std::string s = args[3].getString(token, stack_trace);
-	return sym_t::Number(number_t::Long(mvwprintw(win, y, x, s.c_str())));
-}
-
-ROSSA_EXT_SIG(_addch, args, token, hash, stack_trace)
-{
-	const char c = args[0].getNumber(token, stack_trace).getLong();
-	return sym_t::Number(number_t::Long(addch(c)));
-}
-
-ROSSA_EXT_SIG(_mvaddch, args, token, hash, stack_trace)
-{
-	const int y = args[0].getNumber(token, stack_trace).getLong();
-	const int x = args[1].getNumber(token, stack_trace).getLong();
-	const char c = args[2].getNumber(token, stack_trace).getLong();
-	return sym_t::Number(number_t::Long(mvaddch(y, x, c)));
-}
-
 ROSSA_EXT_SIG(_waddch, args, token, hash, stack_trace)
 {
 	auto win = COERCE_PTR(args[0].getPointer(token, stack_trace), WINDOW);
 	const char c = args[1].getNumber(token, stack_trace).getLong();
 	return sym_t::Number(number_t::Long(waddch(win, c)));
-}
-
-ROSSA_EXT_SIG(_mvwaddch, args, token, hash, stack_trace)
-{
-	auto win = COERCE_PTR(args[0].getPointer(token, stack_trace), WINDOW);
-	const int y = args[1].getNumber(token, stack_trace).getLong();
-	const int x = args[2].getNumber(token, stack_trace).getLong();
-	const char c = args[3].getNumber(token, stack_trace).getLong();
-	return sym_t::Number(number_t::Long(mvwaddch(win, y, x, c)));
-}
-
-ROSSA_EXT_SIG(_getch, args, token, hash, stack_trace)
-{
-	return sym_t::Number(number_t::Long(getch()));
 }
 
 ROSSA_EXT_SIG(_wgetch, args, token, hash, stack_trace)
@@ -193,6 +137,62 @@ ROSSA_EXT_SIG(_typeahead, args, token, hash, stack_trace)
 	return sym_t::Number(number_t::Long(typeahead(args[0].getNumber(token, stack_trace).getLong())));
 }
 
+ROSSA_EXT_SIG(_wmove, args, token, hash, stack_trace)
+{
+	auto win = COERCE_PTR(args[0].getPointer(token, stack_trace), WINDOW);
+	const int y = args[1].getNumber(token, stack_trace).getLong();
+	const int x = args[2].getNumber(token, stack_trace).getLong();
+	return sym_t::Number(number_t::Long(wmove(win, y, x)));
+}
+
+ROSSA_EXT_SIG(_wattron, args, token, hash, stack_trace)
+{
+	auto win = COERCE_PTR(args[0].getPointer(token, stack_trace), WINDOW);
+	const int a = args[1].getNumber(token, stack_trace).getLong();
+	return sym_t::Number(number_t::Long(wattron(win, a)));
+}
+
+ROSSA_EXT_SIG(_wattroff, args, token, hash, stack_trace)
+{
+	auto win = COERCE_PTR(args[0].getPointer(token, stack_trace), WINDOW);
+	const int a = args[1].getNumber(token, stack_trace).getLong();
+	return sym_t::Number(number_t::Long(wattroff(win, a)));
+}
+
+ROSSA_EXT_SIG(_wattrset, args, token, hash, stack_trace)
+{
+	auto win = COERCE_PTR(args[0].getPointer(token, stack_trace), WINDOW);
+	const int a = args[1].getNumber(token, stack_trace).getLong();
+	return sym_t::Number(number_t::Long(wattrset(win, a)));
+}
+
+ROSSA_EXT_SIG(_fetch_constants, args, token, hash, stack_trace)
+{
+	sym_map_t m = {
+		{"NORMAL", sym_t::Number(number_t::Long(A_NORMAL)) },
+		{"ATTRIBUTES", sym_t::Number(number_t::Long(A_ATTRIBUTES))},
+		{"CHARTEXT", sym_t::Number(number_t::Long(A_CHARTEXT))},
+		{"COLOR", sym_t::Number(number_t::Long(A_COLOR))},
+		{"STANDOUT", sym_t::Number(number_t::Long(A_STANDOUT))},
+		{"UNDERLINE", sym_t::Number(number_t::Long(A_UNDERLINE))},
+		{"REVERSE", sym_t::Number(number_t::Long(A_REVERSE))},
+		{"BLINK", sym_t::Number(number_t::Long(A_BLINK))},
+		{"DIM", sym_t::Number(number_t::Long(A_DIM))},
+		{"BOLD", sym_t::Number(number_t::Long(A_BOLD))},
+		{"ALTCHARSET", sym_t::Number(number_t::Long(A_ALTCHARSET))},
+		{"INVIS", sym_t::Number(number_t::Long(A_INVIS))},
+		{"PROTECT", sym_t::Number(number_t::Long(A_PROTECT))},
+		{"HORIZONTAL", sym_t::Number(number_t::Long(A_HORIZONTAL))},
+		{"LEFT", sym_t::Number(number_t::Long(A_LEFT))},
+		{"LOW", sym_t::Number(number_t::Long(A_LOW))},
+		{"RIGHT", sym_t::Number(number_t::Long(A_RIGHT))},
+		{"TOP", sym_t::Number(number_t::Long(A_TOP))},
+		{"VERTICAL", sym_t::Number(number_t::Long(A_VERTICAL))},
+		{"ITALIC", sym_t::Number(number_t::Long(A_ITALIC))}
+	};
+	return sym_t::Dictionary(m);
+}
+
 EXPORT_FUNCTIONS(libncurses)
 {
 	ADD_EXT(_initscr);
@@ -201,13 +201,8 @@ EXPORT_FUNCTIONS(libncurses)
 	ADD_EXT(_delwin);
 	ADD_EXT(_box);
 	ADD_EXT(_wborder);
-	ADD_EXT(_refresh);
 	ADD_EXT(_wrefresh);
-	ADD_EXT(_printw);
 	ADD_EXT(_wprintw);
-	ADD_EXT(_mvprintw);
-	ADD_EXT(_mvwprintw);
-	ADD_EXT(_getch);
 	ADD_EXT(_wgetch);
 	ADD_EXT(_endwin);
 	ADD_EXT(_raw);
@@ -218,8 +213,10 @@ EXPORT_FUNCTIONS(libncurses)
 	ADD_EXT(_noecho);
 	ADD_EXT(_keypad);
 	ADD_EXT(_halfdelay);
-	ADD_EXT(_addch);
-	ADD_EXT(_mvaddch);
 	ADD_EXT(_waddch);
-	ADD_EXT(_mvwaddch);
+	ADD_EXT(_wmove);
+	ADD_EXT(_fetch_constants);
+	ADD_EXT(_wattron);
+	ADD_EXT(_wattroff);
+	ADD_EXT(_wattrset);
 }
